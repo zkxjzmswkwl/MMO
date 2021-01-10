b5e80ed43f8e68fa3b6bc83cdb852d34cac4a1b3
zkxjzmswkwl <smithcarterfreelance@gmail.com>
2021-01-10 02:12:30 -0700

    Added Item base class and ItemType enum.

---
+++ src/main/java/dev/beverlydrive/mmo/items/Item.java
@@ -0,0 +1,22 @@
+package dev.beverlydrive.mmo.items;
+
+public abstract class Item {
+
+    /** TODO:
+     * Much further down the line, I'd like to change these to be
+     * server-sided, and simply store a class of item ids on the client.
+     * Client would then fetch information for each item as-needed from
+     * the server.
+     */
+
+    private String name;
+    private String description;
+    private ItemType type;
+    private int attackBonus;
+    private int strengthBonus;
+    private int defenseBonus;
+    private int rangedBonus;
+    private int magicBonus;
+
+    public abstract void specialAttack();
+}
---
+++ src/main/java/dev/beverlydrive/mmo/items/ItemType.java
@@ -0,0 +1,34 @@
+package dev.beverlydrive.mmo.items;
+
+public enum ItemType {
+    // + Weapons
+    SWORD,
+    DAGGER,
+    BOW,
+    CROSSBOW,
+    STAFF,
+    // - Weapons
+
+    // + Combat consumables
+    ARROW,
+    BOLT,
+    THROWING_AXE,
+    THROWING_KNIFE,
+    RUNE,
+    // - Combat consumables
+
+    // + Herblore
+    VIAL,
+    FILLED_VIAL,
+    MIXED_VIAL,
+    POTION,
+    INGREDIENT,
+    // - Herblore
+
+    // + Containers
+    OREBOX,
+    HERBBASKET,
+    QUIVER,
+    RUNE_POUCH,
+    // -
+}
